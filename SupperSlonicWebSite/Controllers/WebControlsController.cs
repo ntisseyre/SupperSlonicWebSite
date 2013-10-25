68411ee24117c5af33c66350c3c535ed6e30cbc9
supperslonic <natalia.a.zelenskaya@gmail.com>
2013-10-25 14:43:12 -0500

    Web Controls

--- SupperSlonicWebSite/Controllers/WebControlsController.cs
+++ SupperSlonicWebSite/Controllers/WebControlsController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 
 namespace SupperSlonicWebSite.Controllers
 {
-    public class WebDevController : Controller
+    public class WebControlsController : Controller
     {
         public ActionResult Index()
         {
