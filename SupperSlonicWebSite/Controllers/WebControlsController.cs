876eb4d92d8961fca7d15f0d0eab698595263a9d
supperslonic <natalia.a.zelenskaya@gmail.com>
2013-11-11 12:16:43 -0600

    weeklySchedule finished

--- SupperSlonicWebSite/Controllers/WebControlsController.cs
+++ SupperSlonicWebSite/Controllers/WebControlsController.cs
@@ -29,6 +29,9 @@ namespace SupperSlonicWebSite.Controllers
             List<TabWidgetModel> model = new List<TabWidgetModel>(4);
             model.Add(new TabWidgetModel("description", Alignment.Right, "100px"));
             model.Add(new TabWidgetModel("model", Alignment.Right, "201px"));
+            model.Add(new TabWidgetModel("view", Alignment.Right, "302px"));
+            model.Add(new TabWidgetModel("controller", Alignment.Right, "403px"));
+            model.Add(new TabWidgetModel("howToUse", Alignment.Right, "504px"));
             return View(model);
         }
     }
